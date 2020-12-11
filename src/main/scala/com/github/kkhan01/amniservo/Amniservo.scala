60b1015b8fc8ef0d335a166b4659f6ded68b7be7
Khinshan Khan <khanshan501@gmail.com>
2020-12-11 14:56:52 -0500

    mini experiment with one actor per connection

--- src/main/scala/com/github/kkhan01/amniservo/Amniservo.scala
+++ src/main/scala/com/github/kkhan01/amniservo/Amniservo.scala
@@ -4,31 +4,51 @@ import java.net._
 import java.io._
 import scala.io._
 
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+class HelloActor extends Actor {
+  def receive = {
+    case connection: java.net.Socket => {
+
+      val in: Iterator[String] = new BufferedSource(connection.getInputStream()).getLines()
+      val out: java.io.PrintStream = new PrintStream(connection.getOutputStream())
+      var messages: String = ""
+      try {
+        while(true){
+          val input = in.next()
+          messages += input
+          println(messages)
+          out.println(input)
+          out.flush()
+        }
+      } catch {
+        case err: java.util.NoSuchElementException => connection.close()
+        case err: Throwable =>{
+          connection.close()
+          println("Debug: unknown error:")
+          println(err)
+        }
+      }
+    }
+    case _ => println("huh?")
+  }
+}
+
 object Amniserver {
   def server(): Unit = {
     val PORT = 9999
     val bind = new ServerSocket(PORT)
 
-    while (true) {
-      val s = bind.accept()
-      try {
-        while(true){
-          val in = new BufferedSource(s.getInputStream()).getLines()
-          val out = new PrintStream(s.getOutputStream())
+    val system = ActorSystem("HelloSystem")
+    var hi = 1
 
-          val input = in.next()
-          println(input)
-          out.println(input)
-          out.flush()
-        }
-      } catch {
-        case err: java.util.NoSuchElementException => s.close()
-        case err: Throwable =>{
-          s.close()
-          println("Debug: unknown error:")
-          println(err)
-        }
-      }
+    while (true) {
+      val connection: java.net.Socket = bind.accept()
+      val helloActor = system.actorOf(Props[HelloActor], name = hi.toString)
+      hi += 1
+      helloActor ! connection
     }
   }
 }
