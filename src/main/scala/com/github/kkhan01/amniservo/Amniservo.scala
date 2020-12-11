b5d297ba8bfc5c1ce232e9b9c93bd9bc2a6b224b
Khinshan Khan <khanshan501@gmail.com>
2020-12-11 14:06:20 -0500

    socket interface for single connection

--- src/main/scala/com/github/kkhan01/amniservo/Amniservo.scala
+++ src/main/scala/com/github/kkhan01/amniservo/Amniservo.scala
@@ -1,7 +1,24 @@
 package com.github.kkhan01.amniserver
 
+import java.net._
+import java.io._
+import scala.io._
+
 object Amniserver {
-    def server(): Unit = {
-      println("Hello from Server!")
+  def server(): Unit = {
+    val PORT = 9999
+    val bind = new ServerSocket(PORT)
+
+    while (true) {
+      val s = bind.accept()
+      val in = new BufferedSource(s.getInputStream()).getLines()
+      val out = new PrintStream(s.getOutputStream())
+
+      val input = in.next()
+      println(input)
+      out.println(input)
+      out.flush()
+      s.close()
     }
+  }
 }
