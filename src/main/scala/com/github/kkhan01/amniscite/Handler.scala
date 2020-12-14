26dd0261b27283b8425b7fac2db15629eff36861
Khinshan Khan <khanshan501@gmail.com>
2020-12-13 19:03:10 -0500

    wip: decode img

--- src/main/scala/com/github/kkhan01/amniscite/Handler.scala
+++ src/main/scala/com/github/kkhan01/amniscite/Handler.scala
@@ -1,9 +1,12 @@
 package com.github.kkhan01.amniscite
 
+import java.util.Base64
+import java.nio.charset.StandardCharsets
+
 import com.github.kkhan01.amniservo.Amniservo
 
 object Handler {
-  def reverse(queryParams: Map[String, String]): String = {
+  def reverseText(queryParams: Map[String, String]): String = {
     if (queryParams.contains("input")) {
       return queryParams("input").reverse
     }
@@ -12,7 +15,20 @@ object Handler {
     }
   }
 
+  def log(queryParams: Map[String, String]): String = {
+    if (queryParams.contains("img")) {
+      val decoded = Base64.getDecoder().decode(queryParams("img"))
+      val img = new String(decoded, StandardCharsets.UTF_8)
+      println(img)
+      return img
+    }
+    else {
+      throw new Exception("No image string found.");
+    }
+  }
+
   def setup(server: com.github.kkhan01.amniservo.Amniservo) = {
-    server.add("/reverse", reverse)
+    server.add("/reverseText", reverseText)
+    server.add("/log", log)
   }
 }
